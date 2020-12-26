eedf9ee4607efc5eaf37b4c940332cdd4e441301
Sheikh Muhammad Adeel <smaa.link@gmail.com>
2020-12-26 23:23:15 +0500

    added firebase hosting

---
+++ build/precache-manifest.30477d551af733090f682a71aed60a89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a695bed19ff76da0030f72cb5b268b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a200f85e2fe872a64bd",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "df7ef532808ba319aedc",
+    "url": "/static/css/main.e54366a5.chunk.css"
+  },
+  {
+    "revision": "0a200f85e2fe872a64bd",
+    "url": "/static/js/2.4a6181cb.chunk.js"
+  },
+  {
+    "revision": "df7ef532808ba319aedc",
+    "url": "/static/js/main.56878172.chunk.js"
+  },
+  {
+    "revision": "0732ecfcf8703035984a",
+    "url": "/static/js/runtime-main.5019261d.js"
+  },
+  {
+    "revision": "516ff397fa9cfdec2217d250a1ac8f0d",
+    "url": "/static/media/group-icon.516ff397.png"
+  }
+]);
